22b1b25740891bd1d762074ac70af1fe7ffb809e
Luc Anise <lca11@me.com>
2022-05-24 14:57:31 -0400

    added assets List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssetsListComponent } from './components/assets-list/assets-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssetsListComponent
   ],
   imports: [
     BrowserModule,
