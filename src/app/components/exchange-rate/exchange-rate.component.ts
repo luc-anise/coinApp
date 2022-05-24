305cd87d2159b702938a0d0a430ce81cc897796b
Luc Anise <lca11@me.com>
2022-05-24 17:12:58 -0400

    Finished exchange logic

--- src/app/components/exchange-rate/exchange-rate.component.ts
+++ src/app/components/exchange-rate/exchange-rate.component.ts
@@ -5,23 +5,27 @@ import { AssetsService } from 'src/app/services/assets.service';
 @Component({
   selector: 'app-exchange-rate',
   templateUrl: './exchange-rate.component.html',
-  styleUrls: ['./exchange-rate.component.scss']
+  styleUrls: ['./exchange-rate.component.scss'],
 })
 export class ExchangeRateComponent implements OnInit {
-
+  loading = false;
+  rate: number | null = 0;
   exchangeForm = this.fb.group({
     base: [''],
     quote: [''],
   });
 
-  constructor(public assetsService: AssetsService, private fb: FormBuilder) { }
+  constructor(public assetsService: AssetsService, private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   async getQuote() {
-   let quote = await this.assetsService.getExchangeRates(this.exchangeForm.value.base, this.exchangeForm.value.quote);
-    console.log(quote);
+    this.loading = true;
+    let quote = await this.assetsService.getExchangeRates(
+      this.exchangeForm.value.base,
+      this.exchangeForm.value.quote
+    );
+    this.rate = quote.rate;
+    this.loading = false;
   }
-
 }
